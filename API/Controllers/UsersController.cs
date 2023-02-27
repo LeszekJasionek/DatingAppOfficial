66563edbfe111e2feb2039dfdae5f0b7c57662d8
LeszekJas <leszek.jasionek.bee@outlook.com>
2023-02-27 22:34:29 +0100

    4.44 Adding the authentication Middleware

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,10 +1,12 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -12,7 +14,7 @@ namespace API.Controllers
         {
             _context = context;
         }
-
+        [AllowAnonymous]
         [HttpGet(Name = "GetUsers")]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
