79278b9a686d2cb488d8fae6540b06484f386812
Leszek Jasionek <leszek.jasionek.bee@outlook.comgit config --global --listgit config --global user.email leszek.jasionek.bee@outlook.com>
2023-03-08 15:44:22 +0100

    15.184 Adding a message controller

--- API/DTOs/CreateMessageDto.cs
+++ API/DTOs/CreateMessageDto.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.DTOs
 {
-    public class CreateMessage
+    public class CreateMessageDto
     {
         public string RecipientUsername { get; set; }
         public string Content { get; set; }
