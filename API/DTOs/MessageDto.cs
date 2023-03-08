20c32e551c1bdedbe23855dc9f83cbca4d73ad3d
Leszek Jasionek <leszek.jasionek.bee@outlook.comgit config --global --listgit config --global user.email leszek.jasionek.bee@outlook.com>
2023-03-08 15:29:44 +0100

    15.183 Setting up the automapper profiles

--- API/DTOs/MessageDto.cs
+++ API/DTOs/MessageDto.cs
@@ -6,7 +6,7 @@ namespace API.DTOs
         public int SenderId { get; set; }
         public string SenderUsername { get; set; }
 
-        public string SernderPhotoUrl { get; set; }
+        public string SenderPhotoUrl { get; set; }
 
         public int RecipientId { get; set; }
         public string RecipientUsername { get; set; }
