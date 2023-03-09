cf1c53c1851b069ec00dc120b1f2ecbeaed646e6
Leszek Jasionek <leszek.jasionek.bee@outlook.com>
2023-03-09 14:48:40 +0100

    16.201 Configuring the DbContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,16 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Entities;
-using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser, AppRole, int,
+            IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
+            IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
 
         public DataContext(DbContextOptions options) : base(options)
@@ -18,7 +16,7 @@ namespace API.Data
          
         }
 
-        public DbSet<AppUser> Users {get; set;}
+        
         public DbSet<UserLike> Likes {get; set;}
         public DbSet<Message> Messages {get; set;}
 
@@ -26,6 +24,18 @@ namespace API.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+
+            builder.Entity<AppRole>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
 
             builder.Entity<UserLike>()
                 .HasKey(k => new {k.SourceUserId, k.TargetUserId});
