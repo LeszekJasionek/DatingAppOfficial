53b58bb03db356d71e642245d7f48b377a2a480c
Leszek Jasionek <leszek.jasionek.bee@outlook.com>
2023-03-10 14:02:19 +0100

    16. Adding the edit roles component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -27,6 +27,8 @@ import { DatePickerComponent } from './_orms/date-picker/date-picker.component';
 import { MemberMessagesComponent } from './_members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
 
 @NgModule({
   declarations: [
@@ -48,7 +50,9 @@ import { HasRoleDirective } from './_directives/has-role.directive';
     DatePickerComponent,
     MemberMessagesComponent,
     AdminPanelComponent,
-    HasRoleDirective
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent
   ],
   imports: [
     BrowserModule,
