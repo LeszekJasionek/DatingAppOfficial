17068fa7a2273648b62b84f66229d2d792212dfa
Leszek Jasionek <leszek.jasionek.bee@outlook.com>
2023-03-10 13:15:31 +0100

    16.212 Adding an admin guard

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { isArray } from 'ngx-bootstrap/chronos';
 import { BehaviorSubject, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
@@ -37,6 +38,9 @@ export class AccountService {
 
   setCurrentUser(user: User)
   {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles: user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -44,6 +48,10 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null)
+  }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]))
   }
 
 }
