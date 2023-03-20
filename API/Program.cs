d17a156dae7e24ac77bcf5e73b94c184351bd9a3
LeszekJasionek <leszek.jasionek.bee@outlook.com>
2023-03-21 00:23:19 +0100

    19.257 Using git hub actions

--- API/Program.cs
+++ API/Program.cs
@@ -65,11 +65,12 @@ app.UseAuthorization();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-app.MapFallbackToController("Index", "Fallback");
+
 
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 
 using var scope = app.Services.CreateScope();
